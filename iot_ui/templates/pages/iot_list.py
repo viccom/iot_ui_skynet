ef8f4199a1b3054e3856de10d67e2db7a9eccadd
viccom <3520845@qq.com>
2017-06-13 17:34:59 +0800

    add iot_list

---
+++ iot_ui/templates/pages/iot_list.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+import json
+from frappe import _
+from tieta.tieta.doctype.cell_station.cell_station import search_station
+
+
+def get_context(context):
+	if frappe.session.user == 'Guest':
+		frappe.local.flags.redirect_location = "/login"
+		raise frappe.Redirect
+	context.no_cache = 1
+	context.show_sidebar = True
+	context.no_cache = 1
+	menulist = frappe.get_all("Iot Menu")
+	n_list = []
+	for m in menulist:
+		dd = {}
+		dd['url'] = frappe.get_value("Iot Menu", m['name'], "menuurl")
+		dd['name'] = frappe.get_value("Iot Menu", m['name'], "menuname")
+		dd['ico'] = frappe.get_value("Iot Menu", m['name'], "menuico")
+		dd['id'] = frappe.get_value("Iot Menu", m['name'], "ordernum")
+		n_list.append(dd)
+
+	n_list.sort(key=lambda k: (k.get('id', 0)))
+	context.leftnavlist = n_list
+	context.title = _('Devices_List')
