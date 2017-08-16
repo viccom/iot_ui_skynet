eb8f3c2c7a19b8a9edb7f57699ad39746d71927e
viccom <3520845@qq.com>
2017-08-16 12:47:30 +0800

    add

--- iot_ui/templates/pages/iot_console.py
+++ iot_ui/templates/pages/iot_console.py
@@ -129,7 +129,7 @@ def get_context(context):
 						 "latitude": devinfo.latitude})
 			pass
 		pass
-	context.userdevices = {"total":[], "online":[], "offline":[], "offline_7d":[]}
+	context.userdevices = {"total":[0], "online":[0], "offline":[0], "offline_7d":[0]}
 
 	menulist = frappe.get_all("Iot Menu")
 	n_list = []
