e63fd3aaab83e2f09e87d36d68d57959f83da567
viccom <3520845@qq.com>
2017-08-16 12:28:56 +0800

    add

--- iot_ui/templates/pages/iot_console.py
+++ iot_ui/templates/pages/iot_console.py
@@ -129,7 +129,7 @@ def get_context(context):
 						 "latitude": devinfo.latitude})
 			pass
 		pass
-	if userdevices:
+	if userdevices_total:
 		context.userdevices = {"total":userdevices_total,"online":userdevices_online,"offline":userdevices_offline,"offline_7d":userdevices_offline_7d}
 	else:
 		context.userdevices = {"total": "", "online": "", "offline": "", "offline_7d": ""}
