5aa6aabe06ca4cf85aab321be5ff41e9bb2f04f8
viccom <3520845@qq.com>
2018-01-02 12:24:30 +0800

    xxx

--- iot_ui/templates/pages/iot_management.js
+++ iot_ui/templates/pages/iot_management.js
@@ -3,6 +3,21 @@ var symlinksn = '{{ doc.sn }}';
 $(document.body).css({"overflow-y":"scroll" });
 $(document).ready(function() {
 
+    $.ajax({
+        type: 'POST',
+        url: "/api/method/iot.device_api.app_list",
+        Accept: "application/json",
+        contentType: "application/json",
+        data: JSON.stringify({ "device": iotsn}),
+        dataType: "json",
+        success: function(r) {
+          },
+         error: function() {
+              console.log("异常!");
+          }
+    });
+
+
 
     $('#switch-mode').click(function(){
             var url = "/iot_devinfo/" + symlinksn;
