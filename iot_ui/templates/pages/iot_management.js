17c359bba19846beca1ead180aafc537a380c18a
viccom <3520845@qq.com>
2018-01-02 12:29:07 +0800

    xxx

--- iot_ui/templates/pages/iot_management.js
+++ iot_ui/templates/pages/iot_management.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         url: "/api/method/iot.device_api.app_list",
         Accept: "application/json",
         contentType: "application/json",
-        data: JSON.stringify({ "device": iotsn}),
+        data: JSON.stringify({ "device": symlinksn}),
         dataType: "json",
         success: function(r) {
             console.log(r);
