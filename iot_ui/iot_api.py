826b08e17a339559fb2ae55364228a1ffce8774f
Dirk Chang <dirk@kooiot.com>
2018-04-17 17:10:36 +0800

    a few improve on APIs

--- iot_ui/iot_api.py
+++ iot_ui/iot_api.py
@@ -19,104 +19,35 @@ from frappe.utils.user import get_user_fullname
 from frappe.utils import now, get_datetime, convert_utc_to_user_timezone, now_datetime
 
 
-def valid_auth_code(auth_code=None):
-	auth_code = auth_code or frappe.get_request_header("HDB-AuthorizationCode")
-	if not auth_code:
-		throw(_("HDB-AuthorizationCode is required in HTTP Header!"))
-	frappe.logger(__name__).debug(_("HDB-AuthorizationCode as {0}").format(auth_code))
-
-	user = IOTHDBSettings.get_on_behalf(auth_code)
-	if not user:
-		throw(_("Authorization Code is incorrect!"))
-	# form dict keeping
-	form_dict = frappe.local.form_dict
-	frappe.set_user(user)
-	frappe.local.form_dict = form_dict
-
-def list_iot_devices(user):
-	# frappe.logger(__name__).debug(_("List Devices for user {0}").format(user))
-
-	# Get Private Devices
-
-	# pri_devices = frappe.db.get_values("IOT Device", {"owner_id": user, "owner_type": "User"})
-	# print(pri_devices)
-	ent_devices = []
-	groups = _list_user_groups(user)
-	for g in groups:
-		g.group_name = frappe.get_value("Cloud Company Group", g.name, "group_name")
-		gdev = [d[0] for d in
-						frappe.db.get_values("IOT Device", {"owner_id": g.name, "owner_type": "Cloud Company Group"})]
-		ent_devices = ent_devices + gdev
-	pri_devices = [d[0] for d in
-					frappe.db.get_values("IOT Device", {"owner_id": user, "owner_type": "User"})]
-
-	# for c in bunch_codes:
-	# 	pri_devices.append({"bunch": c, "sn": IOTDevice.list_device_sn_by_bunch(c)})
-	devices = {"company_devices": ent_devices, "shared_devices": [], "private_devices": pri_devices}
-	return devices
-
 @frappe.whitelist()
 def ping():
 	return "Pong"
 
 @frappe.whitelist(allow_guest=True)
 def list_devices(user=None):
-	"""
-	List devices according to user specified in query params by naming as 'usr'
-		this user is ERPNext user which you got from @iot.auth.login
-	:param user: ERPNext username
-	:return: device list
-	"""
-	# valid_auth_code()
-	ssuser = frappe.session.user
-	# user = user or frappe.form_dict.get('user')
-	if not ssuser:
-		throw(_("Query string user does not specified"))
+	from iot.hdb_api import list_devices as _list_devices
+	return _list_devices(user)
 
-	return list_iot_devices(ssuser)
 
 @frappe.whitelist()
 def userinfo_all(user):
-	first_name, last_name, avatar, name, language, phone, mobile_no, last_login, last_ip = frappe.db.get_value("User",
-		user, ["first_name", "last_name", "user_image", "name", "language", "phone", "mobile_no", "last_login", "last_ip"])
-	return _dict({
-		"code": 1000,
-		"data":
-			{
-			"fullname": " ".join(filter(None, [first_name, last_name])),
-			"avatar": avatar,
-			"name": name,
-			"language": language,
-			"phone": phone,
-			"mobile_no": mobile_no,
-			"last_login": last_login,
-			"last_ip": last_ip
-			}
-	})
+	return frappe.db.get_value("User", user, as_dict=True,
+		fieldname=["first_name", "last_name", "user_image", "name", "language", "phone", "mobile_no", "last_login", "last_ip"])
 
 
 @frappe.whitelist()
 def iot_device_tree(sn=None):
-	sn = sn or frappe.form_dict.get('sn')
-	doc = frappe.get_doc('IOT Device', sn)
-	doc.has_permission("read")
-	client = redis.Redis.from_url(IOTHDBSettings.get_redis_server() + "/11")
-	subdevice = client.lrange(sn, 0, -1)
+	from iot.hdb import iot_device_tree as _iot_device_tree
+	subdevice = _iot_device_tree(sn)
 	subdevice.remove(sn)
 	return subdevice
 
 
 @frappe.whitelist()
 def iot_device_cfg(sn=None, vsn=None):
-	sn = sn or frappe.form_dict.get('sn')
-	doc = frappe.get_doc('IOT Device', sn)
-	doc.has_permission("read")
-	client = redis.Redis.from_url(IOTHDBSettings.get_redis_server() + "/10")
-	cfg = client.get(vsn or sn)
-	if cfg:
-		return json.loads(cfg)
-	else:
-		return None
+	from iot.hdb import iot_device_cfg as _iot_device_cfg
+	return _iot_device_cfg(sn, vsn)
+
 
 @frappe.whitelist()
 def iot_is_beta(sn=None):
@@ -141,6 +72,7 @@ def get_post_json_data():
 		throw(_("JSON Data not found!"))
 	return frappe._dict(json.loads(frappe.form_dict.data))
 
+
 @frappe.whitelist()
 def get_token():
 	print("WWWWWWWWWWWW")
@@ -159,21 +91,31 @@ def list_company_user(company):
 
 
 @frappe.whitelist()
-def company_admin(user):
+def company_admin():
+	user = frappe.session.user
 	if 'Company Admin' not in frappe.get_roles(user):
-		return False
+		return {"admin": False}
 	return _dict({
-		"code": 1000,
-		"data": list_admin_companies(user)
+		"admin": True,
+		"companies": list_admin_companies(user)
 	})
+
+
+def valid_company_admin(company):
+	if 'Company Admin' not in frappe.get_roles(frappe.session.user) \
+			or not frappe.get_value("Cloud Company", {"name": company, "admin": frappe.session.user, "enabled": 1}):
+		throw(_("You not the admin of company {0}").format(company))
+
+
+def get_company_default_group(company):
+	return frappe.get_value("Cloud Company Group", {"company": company, "group_name": "root"})
+
 
 @frappe.whitelist()
 def list_company_member(company):
-	if 'Company Admin' not in frappe.get_roles(frappe.session.user):
-		return False
-	if frappe.db.get_value("Cloud Company", company, "enabled") != 1:
-		return False
-	return [{"member_id": d[0], "member_name": get_user_fullname(d[0])} for d in frappe.db.get_values("Cloud Employee", {"company": company})]
+	valid_company_admin(company)
+	return [{"user": d[0], "fullname": get_user_fullname(d[0])} for d in frappe.db.get_values("Cloud Employee", {"company": company})]
+
 
 @frappe.whitelist()
 def add_newuser2company():
@@ -181,11 +123,16 @@ def add_newuser2company():
 	company = postdata['company']
 	userinfo = postdata['userinfo']
 	userid = userinfo["email"]
-	group = frappe.get_value("Cloud Company Group", {"company": company, "group_name": "root"})
+
+	valid_company_admin(company)
+	group = get_company_default_group(company)
+	if not group:
+		throw(_("Default group is not found"))
+
 	role = "user"
 	user_exists = frappe.db.get_value("User", {"email": userid}, "email")
-	print("@@@@@@@@@@@@@@@@", user_exists)
 	if not user_exists:
+		frappe.logger(__name__).debug(_("Creating user {0} for company {1}").format(userid, company))
 		user = frappe.new_doc("User")
 		user.update({
 			"name": "New+User+1",
@@ -199,62 +146,72 @@ def add_newuser2company():
 			"mobile_no": userinfo["mobile_no"],
 			"new_password": userinfo["new_password"]
 		})
-		user.insert()
-	user_companies = list_user_companies(userid)
-	if user_companies:
-		if company in user_companies:
-			return {"result": False, "info": userid + " has been an " + company +" Employee"}
-		else:
-			return {"result": False, "info": userid + " has been an other's company Employee"}
-	if not frappe.get_value("Cloud Company", {"name": company, "admin": frappe.session.user}):
-		throw(_("You not the admin of company {0}").format(company))
-	if 'Company Admin' in frappe.get_roles(frappe.session.user):
-		doc = frappe.get_doc({"doctype": "Cloud Employee", "user": userid, "company": company})
-		doc.insert(ignore_permissions=True)
-		g = frappe.get_doc("Cloud Company Group", group)
-		g.add_users(role, userid)
-		return {"result": True, "newuser": userid}
+		user.insert(ignore_permissions=True)
+
+	doc = frappe.get_doc({"doctype": "Cloud Employee", "user": userid, "company": company})
+	doc.insert(ignore_permissions=True)
+	g = frappe.get_doc("Cloud Company Group", group)
+	g.add_users(role, userid)
+	return {"userid": userid}
+
 
 @frappe.whitelist()
 def del_userfromcompany():
 	postdata = get_post_json_data()
 	company = postdata['company']
 	users = postdata['users']
-	group = frappe.get_value("Cloud Company Group", {"company": company, "group_name": "root"})
-	if 'Company Admin' in frappe.get_roles(frappe.session.user):
-		if not frappe.get_value("Cloud Company", {"name": company, "admin": frappe.session.user}):
-			return "You not the admin of company"
-		else:
-			deleted_user = []
-			remained_users = []
-			for m in users:
-				try:
-					g = frappe.get_doc("Cloud Company Group", group)
-					g.remove_users(m)
-					frappe.delete_doc("Cloud Employee", m, ignore_permissions=True)
-					# frappe.delete_doc("User", m)
-					deleted_user.append(m)
-				except Exception as ex:
-					remained_users.append(m)
-			return {"deleted": deleted_user, "remained": remained_users, "result": True}
+
+	valid_company_admin(company)
+	group = get_company_default_group(company)
+
+	deleted_users = []
+	remained_users = []
+	for user in users:
+		try:
+			g = frappe.get_doc("Cloud Company Group", group)
+			g.remove_users(user)
+			frappe.delete_doc("Cloud Employee", user, ignore_permissions=True)
+			deleted_users.append(user)
+		except Exception as ex:
+			remained_users.append(user)
+	return {"deleted": deleted_users, "remained": remained_users}
 
 
 @frappe.whitelist()
 def update_userinfo():
 	postdata = get_post_json_data()
 	userid = postdata["userid"]
+	company = frappe.get_value("Cloud Employee", userid, "company")
+	if not company:
+		throw(_("Invalid employee id {0}").format(userid))
+
+	valid_company_admin(company)
+
 	user = frappe.get_doc("User", userid)
 	user.update({
-		"enabled": 1,
 		"send_welcome_email": 0,
 		"first_name": postdata["first_name"],
 		"last_name": postdata["last_name"],
 		"phone": postdata["phone"],
 		"mobile_no": postdata["mobile_no"],
-		"new_password": postdata["new_password"]
 	})
-	user.save()
-	return {"result": True, "update_info": postdata}
+	if postdata.has_key('enable'):
+		user.update({"enable": postdata.get('enabled')})
+	if postdata.has_key('new_password'):
+		user.update({"new_password": postdata["new_password"]})
+
+	user.save(ignore_permissions=True)
+	return { "userid": userid }
+
+
+@frappe.whitelist()
+def verify_password(password):
+	from frappe.utils.password import check_password
+	try:
+		# returns user in correct case
+		return check_password(frappe.session.user, password)
+	except frappe.AuthenticationError:
+		throw(_("Incorrect password"))
 
 
 @frappe.whitelist()
@@ -380,20 +337,12 @@ def devices_list(filter):
 
 		pass
 
-	rdict = {}
-	rdict["code"] = 1000
 	if filter =="online":
-		if userdevices_online:
-			rdict["data"] = userdevices_online
-		return rdict
+		return userdevices_online or []
 	elif filter =="offline":
-		if userdevices_offline:
-			rdict["data"] = userdevices_offline
-		return rdict
+		return userdevices_offline or []
 	elif filter =="offline_7d":
-		if userdevices_offline_7d:
-			rdict["data"] = userdevices_offline_7d
-		return rdict
+		return userdevices_offline_7d or []
 	elif filter == "len_all":
 		return len(userdevices)
 	elif filter == "len_online":
@@ -405,137 +354,122 @@ def devices_list(filter):
 	elif filter =="devices_amount":
 		return {"all":len(userdevices), "online":len(userdevices_online), "offline":len(userdevices_offline), "offline_7d":len(userdevices_offline_7d)}
 	else:
-		if userdevices:
-			rdict["data"] = userdevices
-		return rdict
+		return userdevices or []
 
 
 @frappe.whitelist()
 def enable_beta(sn):
-	doc = frappe.get_doc("IOT Device", sn);
+	doc = frappe.get_doc("IOT Device", sn)
 	doc.set_use_beta()
 	from iot.device_api import send_action
 	return send_action("sys", action="enable/beta", device=sn, data="1")
+
 
 @frappe.whitelist()
 def add_new_gate(sn, name, desc, owner_type):
 	type = "User"
 	owner = frappe.session.user
-	user_id = frappe.session.user
-	company = list_user_companies(user_id)[0]
-	groupid = None
-	if company:
-		try:
-			groupid = frappe.get_value("Cloud Company Group", {"company": company, "group_name": "root"})
-		except Exception as ex:
-			print(ex)
 	if owner_type == 2:
-		type = "Cloud Company Group"
-		owner = groupid
+		company = list_user_companies(frappe.session.user)[0]
+		try:
+			owner = frappe.get_value("Cloud Company Group", {"company": company, "group_name": "root"})
+			type = "Cloud Company Group"
+			owner = groupid
+		except Exception as ex:
+			throw(_("Cannot find default group in company{0}. Error: {1}").format(company, ex.message))
+
+	iot_device = None
 	sn_exists = frappe.db.get_value("IOT Device", {"sn": sn}, "sn")
-	if sn_exists:
-		if company:
-			try:
-				iot_device = frappe.get_doc("IOT Device", sn)
-			except Exception as ex:
-				print(ex)
-		if iot_device.owner_type:
-			if iot_device.owner_type == "User" and iot_device.owner_id == user_id:
-				return {"result": False, "info": sn + "'s owner is u"}
-			if iot_device.owner_type == "User" and iot_device.owner_id != user_id:
-				if iot_device.owner_id:
-					return {"result": False, "info": sn + "'s owner is not u"}
-			if iot_device.owner_type == "Cloud Company Group" and iot_device.owner_id == groupid:
-				return {"result": False, "info": sn + "'s owner is "+company}
-			if iot_device.owner_type == "Cloud Company Group" and iot_device.owner_id != groupid:
-				if iot_device.owner_id:
-					return {"result": False, "info": sn + "'s owner is not u"}
-		else:
-			try:
-				iot_device.update_owner(type, owner)
-			except Exception as ex:
-				print(ex)
-			return {"result": True, "info": sn}
+	if not sn_exists:
+		iot_device = frappe.get_doc({"doctype": "IOT Device", "sn": sn, "dev_name": name, "description": desc, "owner_type": type, "owner_id": owner})
+		iot_device.insert(ignore_permissions=True)
 	else:
-		doc = frappe.get_doc({"doctype": "IOT Device", "sn": sn, "dev_name": name, "description": desc, "owner_type": type, "owner_id": owner})
-		doc.insert(ignore_permissions=True)
-		return {"result": True, "info": sn}
+		iot_device = frappe.get_doc("IOT Device", sn)
+
+	if not iot_device.owner_id:
+		if iot_device.owner_id == user_id and iot_device.owner_type == type:
+			return True
+		throw(_("Device {0} is owned by {1}").format(sn, iot_device.owner_id))
+	else:
+		iot_device.update_owner(type, owner)
+		return True
+
 
 @frappe.whitelist()
 def remove_gate(sn):
-	try:
-		doc = frappe.get_doc("IOT Device", sn)
-		doc.update_owner("", None)
-		return {"result": True, "sn": sn}
-	except Exception as ex:
-		print(ex)
-		return False
+	doc = frappe.get_doc("IOT Device", sn)
+	doc.update_owner("", None)
+	return True
 
 
 @frappe.whitelist()
 def update_gate(sn, name, desc):
-	try:
-		doc = frappe.get_doc("IOT Device", sn)
-		doc.update({
-			"dev_name": name,
-			"description": desc
-		})
-		doc.save()
-		return {"result": True, "sn": sn}
-	except Exception as ex:
-		print(ex)
-		return False
+	doc = frappe.get_doc("IOT Device", sn)
+	doc.update({
+		"dev_name": name,
+		"description": desc
+	})
+	doc.save()
+	return True
+
 
 @frappe.whitelist()
 def gate_info(sn):
+	device = frappe.get_doc('IOT Device', sn)
+	device.has_permission('read')
+	basic = {
+		'sn': device.sn,
+		'model': "Q102",
+		'name': device.dev_name,
+		'desc': device.description,
+		'company': device.company,
+		'location': device.sn,
+		'beta': device.use_beta,
+		'status': device.device_status,
+	}
+	config = {}
+	applist = {}
+	client = redis.Redis.from_url(IOTHDBSettings.get_redis_server() + "/12")
+	if client.exists(sn):
+		info = client.hgetall()
+		print(info)
+
+		if client.hget(sn, "version/value"):
+			config['iot_version'] = eval(client.hget(sn, "version/value"))[1]
+		if client.hget(sn, "skynet_version/value"):
+			config['skynet_version'] = eval(client.hget(sn, "skynet_version/value"))[1]
+		if client.hget(sn, "starttime/value"):
+			_starttime = eval(client.hget(sn, "starttime/value"))[1]
+			config['starttime'] = str(
+				convert_utc_to_user_timezone(datetime.datetime.utcfromtimestamp(int(_starttime))).replace(
+					tzinfo=None))
+		if client.hget(sn, "uptime/value"):
+			config['uptime'] = int(eval(client.hget(sn, "uptime/value"))[1] / 1000)
+		if client.hget(sn, "skynet_platform/value"):
+			config['skynet_platform'] = eval(client.hget(sn, "skynet_platform/value"))[1]
+
+		s = requests.Session()
+		s.auth = ("api", "Pa88word")
+		r = s.get('http://127.0.0.1:18083/api/v2/nodes/emq@127.0.0.1/clients/' + sn)
+		rdict = json.loads(r.text)
+		config['public_ip'] = rdict['result']['objects'][0]['ipaddress']
+		config['public_port'] = rdict['result']['objects'][0]['port']
+		config['cpu'] = "imx6ull 528MHz"
+		config['ram'] = "256 MB"
+		config['rom'] = "4 GB"
+		config['os'] = "openwrt"
+
 	try:
-		device = frappe.get_doc('IOT Device', sn)
-		device.has_permission('read')
-		gate = {}
-		gate['code'] = 1000
-		gate['data'] = {}
-		gate['data']['basic'] = {}
-		gate['data']['config'] = {}
-		gate['data']['applist'] = {}
-		gate['data']['basic']['sn'] = device.sn
-		gate['data']['basic']['model'] = "Q102"
-		gate['data']['basic']['name'] = device.dev_name
-		gate['data']['basic']['desc'] = device.description
-		gate['data']['basic']['company'] = device.company
-		gate['data']['basic']['Location'] = device.sn
-		gate['data']['basic']['beta'] = device.use_beta
-		gate['data']['basic']['status'] = device.device_status
-		client = redis.Redis.from_url(IOTHDBSettings.get_redis_server() + "/12")
-		if client.exists(sn):
-			if client.hget(sn, "version/value"):
-				gate['data']['config']['iot_version'] = eval(client.hget(sn, "version/value"))[1]
-			if client.hget(sn, "skynet_version/value"):
-				gate['data']['config']['skynet_version'] = eval(client.hget(sn, "skynet_version/value"))[1]
-			if client.hget(sn, "starttime/value"):
-				_starttime = eval(client.hget(sn, "starttime/value"))[1]
-				gate['data']['config']['starttime'] = str(
-					convert_utc_to_user_timezone(datetime.datetime.utcfromtimestamp(int(_starttime))).replace(
-						tzinfo=None))
-			if client.hget(sn, "uptime/value"):
-				gate['data']['config']['uptime'] = int(eval(client.hget(sn, "uptime/value"))[1] / 1000)
-			if client.hget(sn, "skynet_platform/value"):
-				gate['data']['config']['skynet_platform'] = eval(client.hget(sn, "skynet_platform/value"))[1]
-			s = requests.Session()
-			s.auth = ("api", "Pa88word")
-			r = s.get('http://127.0.0.1:18083/api/v2/nodes/emq@127.0.0.1/clients/' + sn)
-			rdict = json.loads(r.text)
-			gate['data']['config']['public_ip'] = rdict['result']['objects'][0]['ipaddress']
-			gate['data']['config']['public_port'] = rdict['result']['objects'][0]['port']
-			gate['data']['config']['cpu'] = "imx6ull 528MHz"
-			gate['data']['config']['ram'] = "256 MB"
-			gate['data']['config']['rom'] = "4 GB"
-			gate['data']['config']['os'] = "openwrt"
-			client = redis.Redis.from_url(IOTHDBSettings.get_redis_server() + "/6")
-			gate['data']['applist'] = json.loads(client.get(sn))
-			return gate
+		client = redis.Redis.from_url(IOTHDBSettings.get_redis_server() + "/6")
+		applist = json.loads(client.get(sn))
 	except Exception as ex:
-		print(ex)
-		return False
+		applist = {}
+
+	return {
+		basic: basic,
+		config: config,
+		applist: app_list,
+	}
 
 
 @frappe.whitelist()
